c8c8e743b8daef6d0b06ed8b7d7a0d68aba32e7b
Nithi krishna <nithikrishna2608@gmail.com>
2026-07-10 04:22:18 +0530

    Fix Render port configuration

--- backend/server.js
+++ backend/server.js
@@ -215,15 +215,11 @@ app.post("/extract", async(req,res)=>{
 
 // Server
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 
-app.listen(PORT,()=>{
-
-
-  console.log(
-    `Server running on http://localhost:${PORT}`
-  );
-
-
+aapp.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
+
